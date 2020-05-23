6e05682d6b31e51329f8890d01ad5d5598d0eebc
Akshaya1102 <39300740+Akshaya1102@users.noreply.github.com>
2020-05-23 21:30:24 +0530

    Add files via upload

---
+++ src/test/java/workshop/Testdemo.java
@@ -0,0 +1,47 @@
+package workshop;
+
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testdemo {
+	
+	@Test
+	public void popUpMethod() throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+
+				
+				System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");	
+				WebDriver driver = new ChromeDriver(); //launch chrome
+				
+				driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+				
+				driver.findElement(By.name("proceed")).click(); //click on Go btn
+				
+				Thread.sleep(5000);
+				
+				Alert alert = driver.switchTo().alert();
+				
+				System.out.println(alert.getText());
+				
+				String text = alert.getText();
+				
+				if(text.equals("Please enter a valid user name")){
+					System.out.println("correct alert messg");
+				}
+				else{
+					System.out.println("in-correct alert messg");
+				}
+				
+				alert.accept(); //click on OK btn
+				
+				//alert.dismiss(); //click on cancel btn
+				
+			}
+
+}
+
+
